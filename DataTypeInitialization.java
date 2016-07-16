6567adfbf5d1eaa996741d1e60e8f41e28f350bc
madhav-jha <madhavjha2008@gmail.com>
2016-07-17 00:02:56 +0530

    Add files via upload

---
+++ DataTypeInitialization.java
@@ -0,0 +1,10 @@
+package Session1.Assignment2;
+
+public class DataTypeInitialization {
+
+	public static void main(String[] args) {
+		int a=1, b=4, c=2;
+		System.out.println("values of a, b, c are "+a+","+b+","+c);
+	}
+
+}
